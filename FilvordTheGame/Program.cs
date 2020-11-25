d4285f1afc20c0642b87b681bd89b4e395ea84c6
AverettF <shuvalov.fedor18@outlook.com>
2020-11-25 14:14:16 +0500

    Добавьте файлы проекта.

---
+++ FilvordTheGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilvordTheGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
