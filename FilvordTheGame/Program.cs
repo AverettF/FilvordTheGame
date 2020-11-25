ad6d878e87d36654d2ab48a56131f0fc395e813b
FedorShuvalov <shuvalov.fedor18@outlook.com>
2020-11-25 14:41:11 +0500

    Print main menu

--- FilvordTheGame/Program.cs
+++ FilvordTheGame/Program.cs
@@ -6,7 +6,20 @@ namespace FilvordTheGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World&!");
+            //ConsoleKeyInfo key;
+            int i = 0;
+            //do
+            //{
+            Menu.Print(i);
+
+            //key = Console.ReadKey();
+            //i = Menu.GetSelectMenu(key, i);
+            //Console.Clear();
+
+            //    if (key.Key == ConsoleKey.Enter)
+            //        SelectMenu(i);
+
+            //} while (!(key.Key == ConsoleKey.Enter && i == 3));
         }
     }
 }
