4ea04b30fd4876ba59b3bc0d018080299f8bd98b
FedorShuvalov <shuvalov.fedor18@outlook.com>
2020-11-25 16:44:07 +0500

    .

--- FilvordTheGame/Menu.cs
+++ FilvordTheGame/Menu.cs
@@ -6,6 +6,37 @@ namespace FilvordTheGame
 {
     class Menu
     {
+        public static void Print(int MenuSelect)
+        {
+            if (MenuSelect == 0)
+                Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\tNew Game\n");
+            Console.ResetColor();
 
+            if (MenuSelect == 1)
+                Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\tContinue \n");
+            Console.ResetColor();
+
+            if (MenuSelect == 2)
+                Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\tRanking\n");
+            Console.ResetColor();
+
+            if (MenuSelect == 3)
+                Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\tExit\n");
+            Console.ResetColor();
+        }
+
+        public static int GetSelectMenu(ConsoleKeyInfo key, int i)
+        {
+            if ((key.Key == ConsoleKey.W || key.Key == ConsoleKey.UpArrow) && (i != 0))
+                return i - 1;
+            else if ((key.Key == ConsoleKey.S || key.Key == ConsoleKey.DownArrow) && (i != 3))
+                return i + 1;
+            else
+                return i;
+        }
     }
 }
